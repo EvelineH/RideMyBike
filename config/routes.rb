23bd2ab31d6c83c6a1f94187b2c54621766371cc
Annelieke Roeleveld <anneliekeroeleveld+github@gmail.com>
2023-01-23 16:41:44 +0100

    finished all models and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_for :users
+  root to: "pages#home"
+
+  resources :bikes do
+    resources :bookings, only: %i[new create]
+  end
+
+  resources :bookings, except: %i[new create destroy]
 end
