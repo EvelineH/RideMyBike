74fc8ad04dae1c6ebb65c557860f23abb66dc923
Evelien Lekkerkerk <e.lekkerkerk@gmail.com>
2023-01-24 09:42:14 +0100

    temporary save, before updating bike model with type_id, category_id, color_id

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -11,6 +11,13 @@ class BikesController < ApplicationController
   end
 
   def create
+    @bike = Bike.new(bike_params)
+    @bike.user = @current_user
+    if @bike.save
+      redirect_to bikes_path(@bikes)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -20,5 +27,11 @@ class BikesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def bike_params
+    params.require(:bike).permit(:type, :category, :color, :location)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Type.create(type: "Classic")
+
+Category.create(category: "Horror")
+Color.create(color: "Romcom")
