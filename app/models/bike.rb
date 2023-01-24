3b8104bff5bbfceebdba7edd9806fc1c5110646e
Evelien Lekkerkerk <e.lekkerkerk@gmail.com>
2023-01-24 13:13:27 +0100

    typo in has_one

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -4,7 +4,7 @@ class Bike < ApplicationRecord
   belongs_to :category
   belongs_to :color
   has_many :users, through: :bookings
-  has_one_attatched :photo
+  has_one_attached :photo
 
   validates :name, presence: true
 end
