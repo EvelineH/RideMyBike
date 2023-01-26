58e27c77ec452ed76cb0419efbdae71a293d63da
Eveline Huiser <evelinehuiser@gmail.com>
2023-01-26 14:42:35 +0100

    booking new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,19 +1,41 @@
 class BookingsController < ApplicationController
+  before_action :set_bike, only: %i[new create]
+
   def index
+    @bookings = Booking.all
   end
 
   def show
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.bike = @bike
+    # @booking.user = current_user
+    if @booking.save
+      redirect_to bookings(@booking)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def set_bike
+    @bike = Bike.find(params[:bike_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_25_104936) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_26_103950) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,11 +68,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_25_104936) do
   create_table "bookings", force: :cascade do |t|
     t.date "start_date"
     t.date "end_date"
-    t.boolean "status", default: false
+    t.boolean "status"
     t.bigint "user_id", null: false
     t.bigint "bike_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "request", default: false
     t.index ["bike_id"], name: "index_bookings_on_bike_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
